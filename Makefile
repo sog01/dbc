30083a55472f6fba420119028a05efb7a22261e1
sog01 <abdullahsog3@gmail.com>
2025-11-06 06:30:07 +0700

    fix: main.c file

--- Makefile
+++ Makefile
@@ -2,17 +2,19 @@ TARGET = bin/dbview
 SRC = $(wildcard src/*.c)
 OBJ = $(patsubst src/%.c, obj/%.o, $(SRC))
 
-run: clean default 
+run: clean default
+	./$(TARGET) -f ./mynewdb.db -n
+	./$(TARGET) -f ./mynewdb.db -a "Timmy H.,123 Sheshire Ln.,120"
 
 default: $(TARGET)
 
 clean:
-	rm -f obj/*.o 
-	rm -f bin/* 
+	rm -f obj/*.o
+	rm -f bin/*
 	rm -f *.db
 
 $(TARGET): $(OBJ)
 	gcc -o $@ $?
 
-obj/%.o: src/%.c
+obj/%.o : src/%.c
 	gcc -c $< -o $@ -Iinclude
--- src/main.c
+++ src/main.c
@@ -9,131 +9,107 @@
 
 void print_usage(char *argv[])
 {
-	printf("Usage: %s -n -f <database file\n", argv[0]);
-	printf("\t -n - create new database file\n");
-	printf("\t -f - (required path to database file\n");
+	printf("Usage: %s -n -f <database file>\n", argv[0]);
+	printf("\t -n  - create new database file\n");
+	printf("\t -f  - (required) path to database file\n");
 	return;
 }
 
 int main(int argc, char *argv[])
 {
-	// char *filepath = NULL;
-	// bool newfile = false;
-	// char *addstring = NULL;
-	// bool list = false;
-	// char *remove = NULL;
-	// char *updatestring = NULL;
-	// int c;
+	char *filepath = NULL;
+	char *portarg = NULL;
+	unsigned short port = 0;
+	bool newfile = false;
+	bool list = false;
+	char *addstring = NULL;
+	int c;
 
-	// int dbfd = -1;
-	// struct dbheader_t *dbhdr = NULL;
-	// struct employee_t *employees = NULL;
+	int dbfd = -1;
+	struct dbheader_t *dbhdr = NULL;
+	struct employee_t *employees = NULL;
 
-	// while ((c = getopt(argc, argv, "nf:a:lr:u:")) != -1)
-	// {
-	// 	switch (c)
-	// 	{
-	// 	case 'n':
-	// 		newfile = true;
-	// 		break;
-	// 	case 'f':
-	// 		filepath = optarg;
-	// 		break;
-	// 	case 'a':
-	// 		addstring = optarg;
-	// 		break;
-	// 	case 'l':
-	// 		list = true;
-	// 		break;
-	// 	case 'r':
-	// 		remove = optarg;
-	// 		break;
-	// 	case 'u':
-	// 		updatestring = optarg;
-	// 		break;
-	// 	case '?':
-	// 		printf("Unknown option -%c\n", c);
-	// 		break;
-	// 	default:
-	// 		return -1;
-	// 	}
-	// }
+	while ((c = getopt(argc, argv, "nf:a:l")) != -1)
+	{
+		switch (c)
+		{
+		case 'n':
+			newfile = true;
+			break;
+		case 'f':
+			filepath = optarg;
+			break;
+		case 'p':
+			portarg = optarg;
+			break;
+		case 'a':
+			addstring = optarg;
+			break;
+		case 'l':
+			list = true;
+			break;
+		case '?':
+			printf("Unknown option -%c\n", c);
+			break;
+		default:
+			return -1;
+		}
+	}
 
-	// if (!filepath)
-	// {
-	// 	printf("Filepath is a required argument\n");
-	// 	print_usage(argv);
-	// 	return 1;
-	// }
+	if (filepath == NULL)
+	{
+		printf("Filepath is a required argument\n");
+		print_usage(argv);
 
-	// if (newfile)
-	// {
-	// 	dbfd = create_db_file(filepath);
-	// 	if (dbfd == STATUS_ERROR)
-	// 	{
-	// 		printf("Unable to create database file\n");
-	// 		return STATUS_ERROR;
-	// 	}
-	// 	if (create_db_header(&dbhdr) == STATUS_ERROR)
-	// 	{
-	// 		printf("Failed to create datbase header\n");
-	// 		return STATUS_ERROR;
-	// 	}
-	// }
-	// else
-	// {
-	// 	dbfd = open_db_file(filepath);
-	// 	if (dbfd == STATUS_ERROR)
-	// 	{
-	// 		printf("Unable to open database file\n");
-	// 		return STATUS_ERROR;
-	// 	}
-	// 	if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR)
-	// 	{
-	// 		printf("Failed to validate database header\n");
-	// 		return STATUS_ERROR;
-	// 	}
-	// }
+		return 0;
+	}
 
-	// if (read_employees(dbfd, dbhdr, &employees) != STATUS_SUCCESS)
-	// {
-	// 	printf("Failed to read employees");
-	// 	return 0;
-	// }
+	if (newfile)
+	{
+		dbfd = create_db_file(filepath);
+		if (dbfd == STATUS_ERROR)
+		{
+			printf("Unable to create database file\n");
+			return -1;
+		}
 
-	// if (addstring)
-	// {
-	// 	add_employee(dbhdr, &employees, addstring);
-	// }
+		if (create_db_header(&dbhdr) == STATUS_ERROR)
+		{
+			printf("Failed to create database header\n");
+			return -1;
+		}
+	}
+	else
+	{
+		dbfd = open_db_file(filepath);
+		if (dbfd == STATUS_ERROR)
+		{
+			printf("Unable to open database file\n");
+			return -1;
+		}
 
-	// if (remove)
-	// {
-	// 	if (remove_employee(dbhdr, employees, remove) == STATUS_SUCCESS)
-	// 	{
-	// 		dbhdr->count--;
-	// 		employees = realloc(employees, dbhdr->count * (sizeof(struct employee_t)));
-	// 		clear_file(dbfd);
-	// 	}
-	// 	else
-	// 	{
-	// 		printf("Employee %s not found\n", remove);
-	// 	}
-	// }
+		if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR)
+		{
+			printf("Failed to validate database header\n");
+			return -1;
+		}
+	}
 
-	// if (updatestring)
-	// {
-	// 	if (update_employee(dbhdr, employees, updatestring) == STATUS_ERROR)
-	// 	{
-	// 		printf("Employee %s not found\n", updatestring);
-	// 	}
-	// }
+	if (read_employees(dbfd, dbhdr, &employees) != STATUS_SUCCESS)
+	{
+		printf("Failed to read employees");
+		return 0;
+	}
 
-	// if (list)
-	// {
-	// 	list_employees(dbhdr, employees);
-	// }
+	if (addstring)
+	{
+		add_employee(dbhdr, &employees, addstring);
+	}
 
-	// output_file(dbfd, dbhdr, employees);
+	if (list)
+	{
+		list_employees(dbhdr, employees);
+	}
 
-	return 0;
+	output_file(dbfd, dbhdr, employees);
 }
