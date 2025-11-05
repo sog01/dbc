cbff2bfe2273c9dbdd5736c13f6116433e7b32ac
sog01 <abdullahsog3@gmail.com>
2025-11-05 16:35:35 +0700

    update

--- include/parse.h
+++ include/parse.h
@@ -22,7 +22,7 @@ struct employee_t
 
 int create_db_header(struct dbheader_t **headerOut);
 int validate_db_header(int fd, struct dbheader_t **headerOut);
-int read_employees(int fd, int test, struct dbheader_t *, struct employee_t **employeesOut);
+int read_employees(int fd, struct dbheader_t *, struct employee_t **employeesOut);
 int add_employee(struct dbheader_t *, struct employee_t **employees, char *addstring);
 int output_file(int fd, struct dbheader_t *, struct employee_t *employees);
 int list_employees(struct dbheader_t *dbhdr, struct employee_t *employees);
