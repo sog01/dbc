a4a2ab1bead9c79c23a5ea2cffe56424e69dcf7e
sog01 <abdullahsog3@gmail.com>
2025-11-05 15:54:28 +0700

    fix: list employee

--- src/parse.c
+++ src/parse.c
@@ -180,6 +180,11 @@ int create_db_header(struct dbheader_t **headerOut)
 
 int list_employees(struct dbheader_t *dbhdr, struct employee_t *employees)
 {
+	if (NULL == dbhdr)
+		return STATUS_ERROR;
+	if (NULL == employees)
+		return STATUS_ERROR;
+
 	int i = 0;
 	for (; i < dbhdr->count; i++)
 	{
@@ -189,5 +194,5 @@ int list_employees(struct dbheader_t *dbhdr, struct employee_t *employees)
 		printf("\tHours: %d\n", employees[i].hours);
 	}
 
-	return 0;
+	return STATUS_SUCCESS;
 }
